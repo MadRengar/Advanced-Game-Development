306c15025dff3addb081feb346f55b77e2598615
MadRengar <1262359229@qq.com>
2025-02-01 20:54:22 +0800

    Add project files

--- SFMLTest/Entity.cpp
+++ SFMLTest/Entity.cpp
@@ -1,28 +1,28 @@
-//#include "Entity.h"
-//#include<stdexcept>
-//
-//Entity::Entity(const std::string& texturePath, const sf::Vector2f& position, const sf::Vector2f& movement)
-//    : movementSpeed(movement) {
-//    if (!texture.loadFromFile(texturePath)) {
-//        throw std::runtime_error("Failed to load texture: " + texturePath);
-//    }
-//    sprite.setTexture(texture);
-//    sprite.setPosition(position);
-//}
-//
-//Entity::~Entity(){}
-//
-//void Entity::draw(Window& window)
-//{
-//    window.draw(sprite);
-//}
-//
-//void Entity::move()
-//{
-//    sprite.move(movementSpeed.x, movementSpeed.y);
-//}
-//
-//sf::Vector2f Entity::getPosition() const
-//{
-//    return sprite.getPosition();
-//}
+#include "Entity.h"
+#include<stdexcept>
+
+Entity::Entity(const std::string& texturePath, const sf::Vector2f& position, const sf::Vector2f& movement)
+    : movementSpeed(movement) {
+    if (!texture.loadFromFile(texturePath)) {
+        throw std::runtime_error("Failed to load texture: " + texturePath);
+    }
+    sprite.setTexture(texture);
+    sprite.setPosition(position);
+}
+
+Entity::~Entity(){}
+
+void Entity::draw(Window& window)
+{
+    window.draw(sprite);
+}
+
+void Entity::move()
+{
+    sprite.move(movementSpeed.x, movementSpeed.y);
+}
+
+sf::Vector2f Entity::getPosition() const
+{
+    return sprite.getPosition();
+}
---
+++ SFMLTest/main.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+
+
+// Check here APIs for the SFML classes to be used in this LAB (all in here: https://www.sfml-dev.org/documentation/2.5.1/annotated.php):
+//   sf::RenderWindow -  https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1RenderWindow.php
+//   sf::Vector2u - https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Vector2.php
+//   sf::Style - https://www.sfml-dev.org/documentation/1.6/namespacesf_1_1Style.php
+//   sf::Drawable - https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Drawable.php
+//   sf::Texture - https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Texture.php 
+//   sf::Sprite - https://www.sfml-dev.org/documentation/2.5.1/classsf_1_1Sprite.php
+
+
+int main()
+{
+    //Declare an object of type "Game"
+    Game game;
+
+    // Simple game loop. Iterate _while_ the window of the game is not "done". Note getWindow() returns a pointer!
+    // Three steps per iteration:
+    //   1. Ask the game instance to handle the input.
+    //   2. Ask the game instance to update its state.
+    //   3. Ask the game instance to render.
+    while (!game.getWindow()->isWindowDone())
+    {
+        game.userInput(); // 处理输入
+        game.update();    // 更新游戏状态
+        game.render();    // 渲染内容
+    }
+
+    return 0;
+}
